4387728140c2e6ca8cbe44c8d60ef17d8a80b882
jifrozen <answldjs1836@naver.com>
2021-04-08 18:01:52 +0900

    user service api 구현

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
-package com.example.restfulwebservice;
+package com.example.restfulwebservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +19,11 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello World");
     }
 
+    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
 
 }
 
---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,69 @@
+package com.example.restfulwebservice.user;
+
+import com.example.restfulwebservice.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service){
+        this.service=service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+
+        if (user==null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+        return user;
+
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser=service.save(user);
+
+        URI location=ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user=service.deleteById(id);
+
+        if(user==null){
+            throw new  UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable int id){
+        Optional<User> userOptional= Optional.ofNullable(service.findOne(id));
+
+        if(!userOptional.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        userOptional.get().setName(user.getName());
+        service.save(userOptional.get());
+
+        return ResponseEntity.noContent().build();
+    }
+}
