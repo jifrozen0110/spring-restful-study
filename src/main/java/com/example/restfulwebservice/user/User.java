20f210dba82c30b9539ff37762e9885e9e55f903
jifrozen <answldjs1836@naver.com>
2021-04-15 01:13:59 +0900

    post rest api 구현

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -9,9 +9,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,8 +24,11 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2,message="Name은 2글자 이상 입력.")
@@ -33,4 +41,15 @@ public class User {
     private String password;
     @ApiModelProperty(notes = "사용자 주민번호을 입력해 주세요")
     private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    public User(int id,String name,Date joinDate,String password,String ssn) {
+        this.id=id;
+        this.name=name;
+        this.joinDate=joinDate;
+        this.password=password;
+        this.ssn=ssn;
+    }
 }
